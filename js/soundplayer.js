4e1c028914f195aaf3149b945713096238f7d684
christdc <cristinadiazcabello@gmail.com>
2024-05-21 22:08:09 +0200

    hecho javascript página principal

---
+++ js/soundplayer.js
@@ -0,0 +1,25 @@
+var audio = document.getElementById("audio");
+var sonido = document.getElementById("sonido");
+var header = document.getElementById("header")
+var corazones = document.getElementsByClassName("soul")
+var nav = document.getElementsByClassName("nav")
+
+setTimeout(function(){
+    header.style.display="block"
+    sonido.play()
+}, 1000)
+
+setTimeout(function(){
+    for (let i = 0; i < nav.length; i++) {
+        nav.item(i).style.display="block"
+     }
+     sonido.play()
+    
+}, 4500)
+
+setTimeout(function(){
+    for (let i = 0; i < corazones.length; i++) {
+        corazones.item(i).style.display="block"
+    }
+    audio.play()
+}, 7000)
