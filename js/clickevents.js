f057b5a0a904f483850ae472aa3208ccc632dd56
christdc <cristinadiazcabello@gmail.com>
2024-05-22 22:20:18 +0200

    terminado trama

--- js/clickevents.js
+++ js/clickevents.js
@@ -1,25 +1,29 @@
 var fight = document.getElementById("botonf");
+var clicked = 0;
+fight.addEventListener("click", function () {
+  switch (clicked % 4) {
+    case 0:
+      fight.style.transform = "translate(20vw, 0%)";
+      break;
+    case 1:
+      fight.style.transform = "translate(0vw, 0%)";
+
+      break;
+    case 2:
+      fight.style.transform = "translate(-20vw, 0%)";
+      break;
+
+    case 3:
+      fight.style.transform = "translate(0vw, 0%)";
+      break;
+  }
+
+  clicked++;
+});
+
+
+var audio = document.getElementById("audio");
 var mercy = document.getElementById("botonm");
-
-fight.addEventListener("click", function() {
-
-    fight.style.transform = 'translate(20vw, 0%)'
-
-    if(fight.style.transform = 'translate(20vw, 0%)'){
-        fight.style.transform = 'translate(0vw, 0%)'
-
-    }else if (fight.style.transform = 'translate(0vw, 0%)') {
-        fight.style.transform = 'translate(-20vw, 0%)'
-    }
-
-    /*if(fight.style.transform = 'translate(-2vw,0)'){
-        fight.style.transform = 'translate(0vw,0)'
-
-    }else if(fight.style.transform = 'translate(0vw,0)'){
-        fight.style.transform = 'translate(2vw,0)'
-
-    }else{
-        fight.style.transform = 'translate(-2vw,0)'
-    }*/
-    
-})
+function play(){
+    audio.play()
+}
