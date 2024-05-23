8cd44e9c6fbc8d0a1a9d40eac7ee5d0b404529aa
christdc <cristinadiazcabello@gmail.com>
2024-05-23 16:26:00 +0200

    arreglado problema con sonido

--- js/soundplayer.js
+++ js/soundplayer.js
@@ -4,16 +4,20 @@ var header = document.getElementById("header")
 var corazones = document.getElementsByClassName("soul")
 var nav = document.getElementsByClassName("nav")
 
-setTimeout(function(){
+setTimeout(async function(){
     header.style.display="block"
+    sonido.muted = false
     sonido.play()
+    sonido.muted = false
 }, 1000)
 
 setTimeout(function(){
     for (let i = 0; i < nav.length; i++) {
         nav.item(i).style.display="block"
      }
+     sonido.muted = false
      sonido.play()
+     sonido.muted = false
     
 }, 4300)
 
@@ -21,5 +25,7 @@ setTimeout(function(){
     for (let i = 0; i < corazones.length; i++) {
         corazones.item(i).style.display="block"
     }
+    audio.muted = false
     audio.play()
+    audio.muted = false
 }, 7000)
