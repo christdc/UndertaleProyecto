d6dba5cd994f8aa56b721e15087f684ac9294cb9
christdc <cristinadiazcabello@gmail.com>
2024-05-24 18:24:47 +0200

    a

--- js/soundclick.js
+++ js/soundclick.js
@@ -14,13 +14,13 @@ chara.addEventListener("click", function(){
         sec.textContent = "Es el primero de los ocho humanos que han caído en el Subsuelo vía el Monte Ebott. Humano que Toriel y Asgore adoptaron al momento de caer en el Subsuelo, y por ende, hermano adoptivo de Asriel Dreemurr. Chara, al final de la Ruta Genocida, él es el deseo de adquirir STATS";
         pulsar.textContent = "PULSA A CHARA!";
         frisk.textContent = "CHARA";
-        chara.src = "../img/chara.png"
+        chara.src = "./img/chara.png"
         
     }else{
         sec.textContent = "Frisk es un personaje jugable y protagonista principal de Undertale. Después de que Frisk cae al Subsuelo, se embarca en un viaje para regresar a la superficie. Frisk es el último humano en caer al Subsuelo después de viajar al Monte Ebott. Frisk no es el humano caído a quien el jugador nombra al comienzo del juego. Su nombre solo se revela en la Ruta Pacifista Verdadera.";
         pulsar.textContent = "PULSA A FRISK!";
         frisk.textContent = "FRISK";
-        chara.src = "../img/frisk.png"
+        chara.src = "./img/frisk.png"
     }
     num++;
 })
